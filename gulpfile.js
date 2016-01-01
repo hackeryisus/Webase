ada0843072a1dd3404430ddd227c33db634a79d3
jemandez <jeman.400@gmail.com>
2015-12-31 20:16:36 -0600

    Se agregan dependencias, index, styles -Se agregaron nuevas dependencias en el archivo packege.json -Gulpfile ahora funciona con la estructura base del proyecto, minifica html y compila sass -Se creo el index con su enlace a la hoja de estilos compilada -Se creo la hoja principal de estilos en scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,33 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
-var stylus = require('gulp-stylus');
-var nib = require('nib');
-var minifyCSS = require('gulp-minify-css');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var uglify = require('gulp-uglify');
-var smoosher = require('gulp-smoosher');
-var imageop = require('gulp-image-optimization');
+var sass = require('gulp-sass');
+var minifyhtml = require('gulp-minify-html');
+
 
 var config = {
   styles: {
-    main:'./src/styles/main.styl',
-    watch: './src/styles/**/*.styl',
-    output: './build/css'
+    main:'./src/styles/main.scss',
+    watch: './src/styles/**/*.scss',
+    output: './dist/css'
   },
   html:{
-    watch: './build/*.html'
+    main: './src/index.html',
+    watch: './src/*.html',
+    output: './dist/'
   },
   scripts:{
     main: './src/scripts/main.js',
     watch: './src/scripts/**/*.js',
-    output: './build/js'
+    output: './dist/js'
   },
   images: {
-  watch: ['./build/img/*.png', './build/img/*.jpg'],
+  watch: ['./src/img/*.png', './src/img/*.jpg'],
   output: './dist/img'
   }
 }
 
 gulp.task('server', function(){
-  gulp.src('./build')
+  gulp.src('./dist')
     .pipe(webserver({
       host:'0.0.0.0',
       port: 8080,
@@ -39,48 +35,19 @@ gulp.task('server', function(){
     }));
 });
 
-gulp.task('build:css', function(){
-  gulp.src(config.styles.main)
-    .pipe(stylus({
-      use: nib(),
-      'include css': true
-    }))
-    .pipe(minifyCSS())
+// Compile sass to css
+gulp.task('build:css', function () {
+  return gulp.src(config.styles.main)
+    .pipe(sass())
     .pipe(gulp.dest(config.styles.output));
 });
 
-gulp.task('build:js', function() {
-  return browserify(config.scripts.main)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(uglify())
-    .pipe(gulp.dest(config.scripts.output));
-})
-
-gulp.task('watch', function(){
-  gulp.watch(config.images.watch, ['images']);
-  gulp.watch(config.scripts.watch, ['build:js']);
-  gulp.watch(config.styles.watch, ['build:css']);
-  gulp.watch(config.html.watch, ['build'] );
+gulp.task('build:html', function () {
+  return gulp.src(config.html.main)
+    .pipe(minifyhtml())
+    .pipe(gulp.dest(config.html.output));
 });
 
-gulp.task('images', function(){
-  gulp.src(config.images.watch)
-    .pipe(imageop({
-      optimizationLevel: 5,
-      progressive: true,
-      interlaced: true
-    }))
-    .pipe(gulp.dest(config.images.output));
-})
+gulp.task('build', ['build:css', 'build:html']);
 
-gulp.task('inline', function(){
-  gulp.src('./build/index.html')
-  .pipe(smoosher())
-  .pipe(gulp.dest('./dist'));
-})
-
-gulp.task('build', ['build:css', 'build:js', 'images', 'inline']);
-
-gulp.task('default', ['server', 'watch', 'build' ]);
+gulp.task('default', ['server','build' ]);
